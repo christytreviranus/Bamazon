18e0f802ea8d4e45e8764290f95e64b3a3422a29
Dark Phoenix <26391122+christytreviranus@users.noreply.github.com>
2019-08-10 08:34:33 -0700

    Removed db specific info

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Wolfg@ng2",
+    password: "enter your password here",
     database: "bamazon"
 });
 
